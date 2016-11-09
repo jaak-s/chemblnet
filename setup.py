6fcb71b4f556af669fb5ff63bb35a4fb55710544
Jaak Simm <jaak.simm@gmail.com>
2016-11-09 15:25:18 +0100

    url for setup.py and README

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='chemblnet',
       version='0.1',
       description='Neural Networks for ChEMBL',
-      url='',
+      url='http://github.com/jaak-s/chemblnet',
       author='Jaak Simm',
       author_email='jaak.simm@gmail.com',
       license='MIT',
