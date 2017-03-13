e4fea1879c9afcc480b96cacd376b4d6af0c483d
Jaak Simm <jaak.simm@gmail.com>
2017-03-13 23:12:42 +0100

    sgld optimizer (not finished)

--- models/macau_sgld.py
+++ models/macau_sgld.py
@@ -20,25 +20,56 @@ p.add("--save",  required=False, type=str, help="filename to save the model to",
 p.add("--save_rmse", required=False, type=str, help="filename to save RMSEs", default = None)
 args = p.parse_args()
 
-#parser = argparse.ArgumentParser()
-#parser.add_argument("--lambda-b", type=float, help="regularization for beta", default = 5.0)
-#parser.add_argument("--lambda-u", type=float, help="regularization for u",    default = 5.0)
-#parser.add_argument("--lambda-v", type=float, help="regularization for v",    default = 5.0)
-#parser.add_argument("--hsize", type=int,   help="size of the hidden layer", default = 100)
-#parser.add_argument("--side",  type=str,   help="side information", default = "chembl-IC50-compound-feat.mm")
-#parser.add_argument("--y",     type=str,   help="matrix", default = "chembl-IC50-346targets.mm")
-#parser.add_argument("--batch-size", type=int,   help="batch size", default = 100)
-#parser.add_argument("--epochs", type=int,  help="number of epochs", default = 200)
-#parser.add_argument("--test-ratio", type=float, help="ratio of y values to move to test set (default 0.20)", default=0.20)
-#parser.add_argument("--noise", type=float, help="Noise multiplier (1.0 is SGLD)", default=1.0)
-#parser.add_argument("--alpha", type=float, help="Noise precisoin (default 5.0)", default=5.0)
-#parser.add_argument("--optimizer", type=str,
-#                    help = "Optimizer to use",
-#                    choices = ["sgd", "sgld"],
-#                    default = "sgd")
-#parser.add_argument("--save",  type=str,   help="filename to save the model to", default = None)
+class SGLD(optimizer.Optimizer):
+    def __init__(self,
+                 learning_rate,
+                 use_locking=False,
+                 name="SGLD"):
+        super(SGLD, self).__init__(use_locking, name)
+        self._opt = tf.train.GradientDescentOptimizer(learning_rate)
+        self._learning_rate = learning_rate
 
-#args = parser.parse_args()
+    def compute_gradients(self, *args, **kwargs):
+        return self._opt.compute_gradients(*args, **kwargs)
+
+    def get_slot(self, *args, **kwargs):
+        return self._opt.get_slot(*args, **kwargs)
+
+    def get_slot_names(self, *args, **kwargs):
+        return self._opt.get_slot_names(*args, **kwargs)
+
+    def apply_gradients(self, grads_and_vars, global_step=None, name=None):
+        with tf.name_scope(name, self._name) as name:
+            update_op = self._opt.apply_gradients(
+                              grads_and_vars, global_step=global_step)
+            add_noise_ops = []
+            with tf.control_dependencies([update_op]):
+                for grad, var in grads_and_vars:
+                    if grad is None:
+                        continue
+                    with tf.name_scope("sgld_noise_" + var.op_name):
+                        if isinstance(grad, ops.Tensor):
+                            add_noise_ops.append(self._noise_dense(var))
+                        else:
+                            add_noise_ops.append(self._noise_sparse(grad, var))
+            ## running combined op
+            return tf.group(*([update_op] + add_noise_ops), name=name)
+
+    def _noise_dense(self, var):
+        with self._maybe_colocate_with(var):
+          updated_var_value = var._ref()  # pylint: disable=protected-access
+          normalized_var = clip_ops.clip_by_norm(
+              updated_var_value, self._max_norm, self._vars_to_clip_dims[var])
+          delta = updated_var_value - normalized_var
+        with ops.colocate_with(var):
+          return var.assign_sub(delta, use_locking=self._use_locking)
+        pass
+
+    def _noise_sparse(self, grad, var):
+        ## TODO: add noise
+        pass
+
+
 
 import tensorflow as tf
 import scipy.io
