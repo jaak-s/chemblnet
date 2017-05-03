25127f6414f4e99ed25b2388ea6fdb6a6f499162
Jaak Simm <jaak.simm@gmail.com>
2017-05-03 17:01:57 +0200

    export pSGLD

--- chemblnet/__init__.py
+++ chemblnet/__init__.py
@@ -1,4 +1,4 @@
 from .vbutils import NormalGammaUni, embedding_lookup_sparse_sumexp
 from .chembl_data import csr2indices, make_train_test, make_target_col, Data
-from .sgld import SGLD, PosteriorMean
+from .sgld import SGLD, pSGLD, PosteriorMean
 from .version import __version__
