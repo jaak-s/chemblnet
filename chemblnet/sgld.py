12be3d28aa639c62a7b31ad31059f4b3d135c493
Jaak Simm <jaak.simm@gmail.com>
2017-05-03 16:57:57 +0200

    pSGLD implementation (dense done, sparse incomplete)

--- chemblnet/sgld.py
+++ chemblnet/sgld.py
@@ -87,3 +87,71 @@ class SGLD(tf.train.Optimizer):
         with colocate_with(var):
             return var.scatter_sub(noise_sparse, use_locking=self._use_locking)
 
+
+class pSGLD(tf.train.Optimizer):
+    """Implements pSGLD using RMSProp optimizer in TF:
+         pcder=(eps + sqrt(state.history));  
+         grad = lr* grad ./ pcder + sqrt(2*lr./pcder).*randn(size(grad))/opts.N ;
+       
+         params = params - grad;
+       where 'state.history' is stored in "rms" slot in RMSProp in TF.
+    """
+
+    def __init__(self,
+                 learning_rate,
+                 use_locking=False,
+                 decay=0.9,
+                 epsilon=1e-10,
+                 temp=1.0,
+                 name="pSGLD"):
+        super(pSGLD, self).__init__(use_locking, name)
+        self._opt = tf.train.RMSProp(learning_rate, decay=decay, epsilon=epsilon)
+        self._learning_rate = learning_rate
+        self._name = name
+        self._use_locking = use_locking
+        self._temp = temp
+        self._epsilon = epsilon
+
+    def compute_gradients(self, *args, **kwargs):
+        return self._opt.compute_gradients(*args, **kwargs)
+
+    def get_slot(self, *args, **kwargs):
+        return self._opt.get_slot(*args, **kwargs)
+
+    def get_slot_names(self, *args, **kwargs):
+        return self._opt.get_slot_names(*args, **kwargs)
+
+    def apply_gradients(self, grads_and_vars, global_step=None, name=None):
+        with tf.name_scope(name, self._name) as name:
+            update_op = self._opt.apply_gradients(
+                              grads_and_vars, global_step=global_step)
+            add_noise_ops = []
+            with tf.control_dependencies([update_op]):
+                for grad, var in grads_and_vars:
+                    if grad is None:
+                        continue
+                    with tf.name_scope("psgld_noise_" + var.op.name):
+                        if isinstance(grad, tf.Tensor):
+                            add_noise_ops.append(self._noise_dense(var))
+                        else:
+                            add_noise_ops.append(self._noise_sparse(grad, var))
+            ## running combined op
+            return tf.group(*([update_op] + add_noise_ops), name=name)
+
+    def _noise_dense(self, var):
+        updated_var_value = var._ref()  # pylint: disable=protected-access
+        pcder = tf.sqrt(self._opt.get_slot(var, name="rms") + self._epsilon)
+        noise = tf.random_normal(shape = tf.shape(var), stddev = self._temp * tf.sqrt(2 * self._learning_rate / pcder))
+        with colocate_with(var):
+            return var.assign_add(noise, use_locking=self._use_locking)
+
+    def _noise_sparse(self, grad, var):
+        assert isinstance(grad, tf.IndexedSlices)
+
+        ## TODO: using pcder only update slices
+
+        noise = tf.random_normal(shape = tf.shape(grad.values), stddev = self._temp * tf.sqrt(2 * self._learning_rate))
+        noise_sparse = tf.IndexedSlices(noise, grad.indices, grad.dense_shape)
+
+        with colocate_with(var):
+            return var.scatter_sub(noise_sparse, use_locking=self._use_locking)
